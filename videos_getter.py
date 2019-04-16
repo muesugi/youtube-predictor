9f963190d9ca36df2038e1e14aaecf1856fe73df
muesugi <moie.uesugi@gmail.com>
2019-04-16 14:52:57 -0400

    cloudsql connection to pull data from video_data db

---
+++ videos_getter.py
@@ -0,0 +1,44 @@
+import os
+import json
+import config
+import pymysql
+import argparse
+import sys
+
+connection = pymysql.connect(host='127.0.0.1',
+                            port = 3305,
+                            user = config.USER,
+                            password = config.PASSWORD,
+                            db = 'video_data')
+
+def create_tables():
+    # Create tables (if they don't already exist)
+    # TODO: create features table once we have all the features? or
+    # create a table for each feature?
+    # try:
+    #     conn.execute(
+    #         "CREATE TABLE IF NOT EXISTS features "
+    #         + "(id VARCHAR(20) PRIMARY KEY, channelId TEXT, "
+    #         + "playlistId TEXT, channelTitle TEXT, "
+    #         + "title TEXT, description TEXT, duration TEXT, "
+    #         + "categoryId INT, publishedAt TEXT, defaultAudioLanguage TEXT, "
+    #         + "likeCount INT, dislikeCount INT, viewCount INT, commentCount INT);"
+    #     )
+    # except TypeError as e:
+    #     print(e)
+    #     return None
+
+def query_videos(sqlquery, args=None, fetch_one=False):
+    with connection.cursor() as cursor:
+        if args:
+            cursor.execute(sqlquery, args)
+        else:
+            cursor.execute(sqlquery)
+
+        if fetch_one:
+            return [cursor.fetchone()]
+        else:
+            return cursor.fetchall()
+
+if __name__ == '__main__':
+    print(query_videos("SELECT title FROM videos;"))
